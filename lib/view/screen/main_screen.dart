33d745259ebab843768fef6563542e7578aa7e94
Samir_N_Ahmad <sameer.ahmad1024@gmail.com>
2020-11-19 12:07:38 +0200

    UMS-1 Implement main screen basic design.

---
+++ lib/view/screen/main_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Color(0XFFEEEEEE),
+        appBar: AppBar(
+          title: Text(
+            "Hi, Samir",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.notifications_on),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.search),
+            ),
+            IconButton(
+              onPressed: null,
+              icon: Icon(Icons.download_done_sharp),
+            )
+          ],
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ),
+            Expanded(
+                flex: 9,
+                child: GridView.builder(
+                    padding: EdgeInsets.all(10),
+                    itemCount: 6,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: MediaQuery.of(context).orientation ==
+                                Orientation.landscape
+                            ? 4
+                            : 2,
+                        crossAxisSpacing: 20,
+                        childAspectRatio: 1,
+                        mainAxisSpacing: 20),
+                    itemBuilder: (context, index) => Container(
+                          padding: EdgeInsets.all(5),
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: Color(0x77B2B2B2),
+                                    blurRadius: 10,
+                                    spreadRadius: 3)
+                              ]),
+                        )))
+          ],
+        ));
+  }
+}
