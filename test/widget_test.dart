750a7624c6901c610515ec725a3d35263bf9f0d9
Samir_N_Ahmad <sameer.ahmad1024@gmail.com>
2020-11-26 21:44:55 +0200

    develop: Change app's namespace to university_management_system.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:u/main.dart';
+import 'package:university_management_system/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
